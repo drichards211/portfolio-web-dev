7b04e33ac994157feae04c27051386346c5620e5
Daniel Richards <drichards211@yahoo.com>
2019-07-17 22:22:14 -0400

    Add handleSiteNav() function

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+function handleSiteNav() {
+// Respond to user button presses:
+  console.log('handleSiteNav() running')
+  $('body').on('click', 'button', function(event) {
+    // portfolio button pressed:
+    if (`${$(this).prop('id')}` === 'portfolio') {
+      console.log("portfolio button pressed")
+      // scroll to beginning of portfolio section:  
+      $('html, body').animate({
+        scrollTop: ($('.spring').offset().top)
+    },800);
+    } 
+    // seasons-thumbnail button pressed:
+    if (`${$(this).prop('id')}` === 'seasons-thumbnail') {
+      console.log("seasons-thumbnail button pressed")
+      // scroll to top of page:
+      $('html, body').animate({
+        scrollTop: ($('html, body').offset().top)
+    },800);
+    } 
+    // hamburger button pressed:
+      if (`${$(this).prop('id')}` === 'hamburger') {
+      console.log("hamburger button pressed")
+      // display hamburger dropdown menu:
+      $('html, body').animate({
+        scrollTop: ($('.spring').offset().top)
+    },800);
+    } 
+  })
+}
+
 function centerNameVertically() {
 // Centers name vertically within the hero image:
   console.log("centerNameVertically() running")
@@ -21,4 +52,5 @@ function centerNameVertically() {
 
 $(function() {
   centerNameVertically()
+  handleSiteNav()
 })
