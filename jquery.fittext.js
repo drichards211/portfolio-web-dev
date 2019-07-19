b166a628cfb70421941cad08a40c6c11222c6bb8
Daniel Richards <drichards211@yahoo.com>
2019-07-19 15:58:15 -0400

    Allow fitText.js to call centerNameVertically()

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -11,7 +11,8 @@
 
 (function( $ ){
 
-  $.fn.fitText = function( kompressor, compLevel, options ) {
+  $.fn.fitText = function( kompressor, compLevel, options ) { 
+    // DR added compLevel to fine-tune font-sizing on a case-by-case basis
 
     // Setup options
     var compressor = kompressor || 1,
@@ -19,7 +20,7 @@
           'minFontSize' : Number.NEGATIVE_INFINITY,
           'maxFontSize' : Number.POSITIVE_INFINITY
         }, options);
-    var compCalc = compLevel || 'compressor * 13.5'
+    var compCalc = compLevel || 'compressor * 10'
     return this.each(function(){
 
       // Store the object
@@ -28,6 +29,7 @@
       // Resizer() resizes items based on the object width divided by the compressor * 10
       var resizer = function () {
         $this.css('font-size', Math.max(Math.min($this.width() / (eval(compCalc)), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+        centerNameVertically() // in script.js
       };
       //compressor*13.5
 
--- script.js
+++ script.js
@@ -34,17 +34,12 @@ function centerNameVertically() {
   console.log("centerNameVertically() running")
   setTimeout(function() {
   // wait 0.3 seconds for .fitText to finish adjusting text-size before executing:
-    console.log("Re-centering name vertically inside hero image")
     let nameHeight1 = $(".name").outerHeight() + "px"
-  $(".name").css({"top": `calc(50% - (${nameHeight1} / 2))`})
-  $(".name").css({"transition": "top 0.5s"})
+    $(".name").css({"top": `calc(50% - (${nameHeight1} / 2))`})
+    $(".name").css({"transition": "top 0.5s"})
   }, 300)
-  window.onresize = function() {
-    centerNameVertically()
-  }
 }
 
 $(function() {
-  centerNameVertically()
   handleSiteNav()
 })
