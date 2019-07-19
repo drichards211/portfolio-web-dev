7621ef91093090e2ee22562662690622a612a427
Daniel Richards <drichards211@yahoo.com>
2019-07-19 15:41:37 -0400

    Darken background-color and box-shadow on .name

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function centerNameVertically() {
   console.log("centerNameVertically() running")
   setTimeout(function() {
   // wait 0.3 seconds for .fitText to finish adjusting text-size before executing:
-    console.log("Re-centering name verticaly inside hero image")
+    console.log("Re-centering name vertically inside hero image")
     let nameHeight1 = $(".name").outerHeight() + "px"
   $(".name").css({"top": `calc(50% - (${nameHeight1} / 2))`})
   $(".name").css({"transition": "top 0.5s"})
